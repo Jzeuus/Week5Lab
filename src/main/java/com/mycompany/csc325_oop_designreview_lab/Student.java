cb6c4dd9a15f2a8cd66ea9f54e1676b931558f81
Jesus Alvarado <inkakola9@gmail.com>
2022-03-01 14:49:37 -0500

    ToDo2: Fixed the resulting errors

--- src/main/java/com/mycompany/csc325_oop_designreview_lab/Student.java
+++ src/main/java/com/mycompany/csc325_oop_designreview_lab/Student.java
@@ -10,6 +10,21 @@ package com.mycompany.csc325_oop_designreview_lab;
  * @author MoaathAlrajab
  */
 public class Student extends Human{
+    private String address;
+    public Student(String name, short age){
+        
+        super(name, age);
+    }
+
+    @Override
+    public String getAddress() {    
+        return address;
+    }
+
+    @Override
+    public void setAddress(String address) {
+        this.address = address;
+    }
 	// ToDo 1: Make this class a child of Human
 	
 	// ToDo 2: Fix the resulting errors
