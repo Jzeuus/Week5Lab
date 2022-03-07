3f3ea605bdf3355ff8151b211cb11afbc02890c6
Jesus Alvarado <inkakola9@gmail.com>
2022-03-07 18:00:07 -0500

    ToDo4: Added comments to the code

--- src/main/java/com/mycompany/csc325_oop_designreview_lab/Student.java
+++ src/main/java/com/mycompany/csc325_oop_designreview_lab/Student.java
@@ -2,34 +2,69 @@
 package com.mycompany.csc325_oop_designreview_lab;
 
 /**
- *
- * @author MoaathAlrajab
+ * Student --
+ * child class of the Human class, extends and implements the 
+ * properties of the human class, while implementing its own fields 
+ * pertaining to a student object.
+ * 
+ * @author MoaathAlrajab, Jesus Alvarado
  */
-/**
- *
- * @author MoaathAlrajab
- */
+
 public class Student extends Human{
+  
     private double gpa;
+    
+    /** 
+     * Student--
+     * Constructor method for instantiating the Student object
+     * @param: String name, short age
+     * @return: string address
+     */
     public Student(String name, short age){
         
         super(name, age);
     }
 
+    /**
+     * getAddress--
+     * getter method for getting a student's address. Implements and overrides
+     * the method provided by the parent class
+     * @param: 
+     * @return: string address
+     */
     @Override
     public String getAddress() {    
         return super.address;
     }
 
+    /** 
+     * setAddress--
+     * setter method for setting a student's address. Implements and overrides
+     * the method provided by the parent class
+     * @param: string address
+     * @return: 
+     */
     @Override
     public void setAddress(String address) {
         super.address = address;
     }
     
+    /** 
+     * getGPA--
+     * getter method for getting a student's gpa
+     * @param: 
+     * @return: double gpa
+     */
     public double getGPA(){
         return gpa;
     }
     
+    /**
+     * setGPA --
+     * setter method for setting a student's gpa
+     * @param: double grade
+     * @return: void
+     */
     public void setGPA(double grade){
         gpa = grade;
     }
