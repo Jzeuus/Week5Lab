3effd0f3514f93ea9e69254ddef06d5694857158
Jesus Alvarado <inkakola9@gmail.com>
2022-03-07 18:18:41 -0500

    ToDo7: created a Freshman and a Senior class

---
+++ src/main/java/com/mycompany/csc325_oop_designreview_lab/Senior.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.csc325_oop_designreview_lab;
+
+/**
+ *
+ * @author jesusalvarado
+ */
+public class Senior extends Student{
+    
+    public Senior(String name, short age, double grade) {
+        super(name, age, grade);
+    }
+    
+}
