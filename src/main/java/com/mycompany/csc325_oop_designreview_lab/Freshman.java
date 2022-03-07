5c06c948a1fc1d1f4f9982f0dbe29f6d8d243c85
Jesus Alvarado <inkakola9@gmail.com>
2022-03-07 18:33:52 -0500

    Had to redo the fixes for ToDo5, ToDo6, and ToDo7, to correctly fix the problem.

---
+++ src/main/java/com/mycompany/csc325_oop_designreview_lab/Freshman.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.csc325_oop_designreview_lab;
+
+/**
+ *
+ * @author jesusalvarado
+ */
+public class Freshman extends Student{
+    private int credits;
+    public Freshman(String name, short age, int credits) {
+        super(name, age);
+    }
+    public int getCredits(){
+        return credits;
+    }
+    
+    public void setCredits(int newCredits){
+        credits = newCredits;
+    }
+    
+}
